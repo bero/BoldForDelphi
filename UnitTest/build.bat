16254ad68f7dcf6b8a0e43cc256cebee4e86f962
Roland Bengtsson <roland.bengtsson@aholadigital.com>
2025-12-11 14:37:25 +0200

    Made Build.bat smarted to detect Delphi-version

--- UnitTest/build.bat
+++ UnitTest/build.bat
@@ -1,3 +1,24 @@
 @echo off
-call "C:\Program Files (x86)\Embarcadero\Studio\23.0\bin\rsvars.bat"
-msbuild "C:\Attracs\BoldForDelphi\UnitTest\UnitTest.dproj" /p:Config=Debug /p:Platform=Win32 /v:minimal
+setlocal enabledelayedexpansion
+
+REM Find the latest installed Delphi version
+set "STUDIO_BASE=C:\Program Files (x86)\Embarcadero\Studio"
+set "LATEST_VERSION="
+
+if not exist "%STUDIO_BASE%" (
+    echo Error: Cannot find Embarcadero Studio installation
+    exit /b 1
+)
+
+pushd "%STUDIO_BASE%"
+for /d %%d in (*) do if exist "%%d\bin\rsvars.bat" set "LATEST_VERSION=%%d"
+popd
+
+if "!LATEST_VERSION!"=="" (
+    echo Error: No Delphi installation found
+    exit /b 1
+)
+
+echo Using Delphi installation: %STUDIO_BASE%\!LATEST_VERSION!
+call "%STUDIO_BASE%\!LATEST_VERSION!\bin\rsvars.bat"
+msbuild "%~dp0UnitTest.dproj" /p:Config=Debug /p:Platform=Win32 /v:minimal
