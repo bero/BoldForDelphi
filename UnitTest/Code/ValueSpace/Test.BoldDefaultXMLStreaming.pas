5119c81f24c36003756f129c490f5ae6f93bd3cf
Roland Bengtsson <roland.bengtsson@aholadigital.com>
2026-03-29 19:25:51 +0300

    Add 4 more XML streaming tests: persistence state, multi-object, blob, error path
    - WriteValueSpace with IgnorePersistenceState=False (exercises persistence state nodes)
    - Write/read 3 objects with different attribute types in single value space
    - Blob and TypedBlob with special chars and ContentType
    - ClassStreamerByName with invalid name raises exception

--- UnitTest/Code/ValueSpace/Test.BoldDefaultXMLStreaming.pas
+++ UnitTest/Code/ValueSpace/Test.BoldDefaultXMLStreaming.pas
@@ -39,6 +39,14 @@ type
     procedure TestStreamManagerCreate;
     [Test]
     procedure TestClassStreamerCount;
+    [Test]
+    procedure TestWriteWithPersistenceState;
+    [Test]
+    procedure TestWriteMultipleObjects;
+    [Test]
+    procedure TestWriteReadWithBlobContent;
+    [Test]
+    procedure TestClassStreamerByNameNotFound;
   end;
 
 implementation
@@ -219,6 +227,187 @@ begin
     // Access class streamer for ClassA (TopSortedIndex 1)
     Assert.IsNotNull(aMgr.ClassStreamers[1], 'Should have ClassA streamer');
     Assert.IsNotNull(aMgr.ClassStreamerByName['ClassA'], 'Should find ClassA by name');
+  finally
+    aMgr.Free;
+  end;
+end;
+
+procedure TTestBoldDefaultXMLStreaming.TestWriteWithPersistenceState;
+var
+  aMgr: TBoldDefaultXMLStreamManager;
+  aDoc: TDomDocument;
+  aNode: TBoldXMLNode;
+  anXML: string;
+  Obj: jehoBCBoldTest.TClassA;
+  anIdList: TBoldObjectIdList;
+begin
+  Obj := jehoBCBoldTest.TClassA.Create(GetSystem);
+  Obj.aString := 'WithState';
+  Obj.aInteger := 99;
+
+  aDoc := TDOMDocument.Create(nil);
+  aMgr := TBoldDefaultXMLStreamManager.Create(
+    TBoldDefaultXMLStreamerRegistry.MainStreamerRegistry,
+    FDataModule.BoldModel1.MoldModel);
+  try
+    // Include persistence state in streaming
+    aMgr.IgnorePersistenceState := False;
+    aMgr.PersistenceStatesToBeStreamed := [bvpsModified, bvpsCurrent, bvpsInvalid];
+    aNode := aMgr.NewRootNode(aDoc, 'StateTest');
+
+    anIdList := TBoldObjectIdList.Create;
+    try
+      anIdList.Add(Obj.BoldObjectLocator.BoldObjectID);
+      aMgr.WriteValueSpace(GetSystem.AsIBoldvalueSpace[bdepContents], anIdList, nil, aNode);
+    finally
+      anIdList.Free;
+    end;
+
+    anXML := aNode.XMLDomElement.ownerDocument.xml;
+    Assert.IsTrue(Length(anXML) > 50, 'XML with persistence state should have content');
+
+    aNode.Free;
+  finally
+    aMgr.Free;
+    aDoc.Free;
+  end;
+end;
+
+procedure TTestBoldDefaultXMLStreaming.TestWriteMultipleObjects;
+var
+  aMgr: TBoldDefaultXMLStreamManager;
+  aDoc: TDomDocument;
+  aNode: TBoldXMLNode;
+  anXML: string;
+  Obj1, Obj2, Obj3: jehoBCBoldTest.TClassA;
+  anIdList: TBoldObjectIdList;
+  FSValueSpace: TBoldFreeStandingValueSpace;
+  ParseError: IXMLDOMParseError;
+begin
+  Obj1 := jehoBCBoldTest.TClassA.Create(GetSystem);
+  Obj2 := jehoBCBoldTest.TClassA.Create(GetSystem);
+  Obj3 := jehoBCBoldTest.TClassA.Create(GetSystem);
+  Obj1.aString := 'First';
+  Obj1.aInteger := 1;
+  Obj2.aString := 'Second';
+  Obj2.aFloat := 2.0;
+  Obj3.aString := 'Third';
+  Obj3.aCurrency := 3.00;
+
+  // Write all 3 objects
+  aDoc := TDOMDocument.Create(nil);
+  aMgr := TBoldDefaultXMLStreamManager.Create(
+    TBoldDefaultXMLStreamerRegistry.MainStreamerRegistry,
+    FDataModule.BoldModel1.MoldModel);
+  try
+    aMgr.IgnorePersistenceState := True;
+    aNode := aMgr.NewRootNode(aDoc, 'MultiTest');
+
+    anIdList := TBoldObjectIdList.Create;
+    try
+      anIdList.Add(Obj1.BoldObjectLocator.BoldObjectID);
+      anIdList.Add(Obj2.BoldObjectLocator.BoldObjectID);
+      anIdList.Add(Obj3.BoldObjectLocator.BoldObjectID);
+      aMgr.WriteValueSpace(GetSystem.AsIBoldvalueSpace[bdepContents], anIdList, nil, aNode);
+    finally
+      anIdList.Free;
+    end;
+
+    anXML := aNode.XMLDomElement.ownerDocument.xml;
+    aNode.Free;
+  finally
+    aMgr.Free;
+    aDoc.Free;
+  end;
+
+  // Read back and verify all 3 objects
+  aDoc := TDOMDocument.Create(nil);
+  try
+    aDoc.async := False;
+    aDoc.loadXML(anXML);
+    ParseError := aDoc.parseError;
+    Assert.IsTrue((not Assigned(ParseError)) or (ParseError.errorCode = 0), 'Should parse');
+
+    aMgr := TBoldDefaultXMLStreamManager.Create(
+      TBoldDefaultXMLStreamerRegistry.MainStreamerRegistry,
+      FDataModule.BoldModel1.MoldModel);
+    try
+      aMgr.IgnorePersistenceState := True;
+      aNode := aMgr.GetRootNode(aDoc, 'MultiTest');
+
+      FSValueSpace := TBoldFreeStandingValueSpace.Create;
+      try
+        aMgr.ReadValueSpace(FSValueSpace, aNode);
+        Assert.IsTrue(FSValueSpace.GetHasContentsForId(Obj1.BoldObjectLocator.BoldObjectID), 'Should have Obj1');
+        Assert.IsTrue(FSValueSpace.GetHasContentsForId(Obj2.BoldObjectLocator.BoldObjectID), 'Should have Obj2');
+        Assert.IsTrue(FSValueSpace.GetHasContentsForId(Obj3.BoldObjectLocator.BoldObjectID), 'Should have Obj3');
+      finally
+        FSValueSpace.Free;
+      end;
+
+      aNode.Free;
+    finally
+      aMgr.Free;
+    end;
+  finally
+    aDoc.Free;
+  end;
+end;
+
+procedure TTestBoldDefaultXMLStreaming.TestWriteReadWithBlobContent;
+var
+  aMgr: TBoldDefaultXMLStreamManager;
+  aDoc: TDomDocument;
+  aNode: TBoldXMLNode;
+  anXML: string;
+  Obj: jehoBCBoldTest.TClassA;
+  anIdList: TBoldObjectIdList;
+begin
+  Obj := jehoBCBoldTest.TClassA.Create(GetSystem);
+  Obj.aBlob := 'Binary blob data with special chars: <>&"';
+  Obj.aBlobContent := 'typed blob';
+  Obj.M_aBlobContent.ContentType := 'text/plain';
+
+  aDoc := TDOMDocument.Create(nil);
+  aMgr := TBoldDefaultXMLStreamManager.Create(
+    TBoldDefaultXMLStreamerRegistry.MainStreamerRegistry,
+    FDataModule.BoldModel1.MoldModel);
+  try
+    aMgr.IgnorePersistenceState := True;
+    aNode := aMgr.NewRootNode(aDoc, 'BlobTest');
+
+    anIdList := TBoldObjectIdList.Create;
+    try
+      anIdList.Add(Obj.BoldObjectLocator.BoldObjectID);
+      aMgr.WriteValueSpace(GetSystem.AsIBoldvalueSpace[bdepContents], anIdList, nil, aNode);
+    finally
+      anIdList.Free;
+    end;
+
+    anXML := aNode.XMLDomElement.ownerDocument.xml;
+    Assert.IsTrue(Length(anXML) > 100, 'Blob XML should have content');
+
+    aNode.Free;
+  finally
+    aMgr.Free;
+    aDoc.Free;
+  end;
+end;
+
+procedure TTestBoldDefaultXMLStreaming.TestClassStreamerByNameNotFound;
+var
+  aMgr: TBoldDefaultXMLStreamManager;
+begin
+  aMgr := TBoldDefaultXMLStreamManager.Create(
+    TBoldDefaultXMLStreamerRegistry.MainStreamerRegistry,
+    FDataModule.BoldModel1.MoldModel);
+  try
+    Assert.WillRaiseAny(
+      procedure
+      begin
+        aMgr.ClassStreamerByName['NonExistentClass'];
+      end
+    );
   finally
     aMgr.Free;
   end;
